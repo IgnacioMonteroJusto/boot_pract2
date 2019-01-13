47988b18f127fa52cb2ea02f27c3651771838477
nachoGotaki <ignacio.montero@gotaki.com>
2019-01-13 13:59:56 +0100

    compile y deploy olvidados

--- compile.js
+++ compile.js
@@ -4,25 +4,20 @@ const solc = require("solc");
 
 //con esta variable nos posicionamos desde la raiz del proyecto hasta inbox.sol
 const inboxPath = path.resolve(__dirname, "contracts", "campaign.sol");
-//const inboxPath = path.resolve(__dirname, "contracts", "campaign.sol");
 const source = fs.readFileSync(inboxPath, "utf-8");
 
+const inboxProxyPath = path.resolve(__dirname, "contracts", "proxy.sol");
+const sourceProxy = fs.readFileSync(inboxProxyPath, "utf-8");
 
+const inboxTokenPath = path.resolve(__dirname, "contracts", "firsttoken.sol");
+const sourceToken = fs.readFileSync(inboxTokenPath, "utf-8");
 
-/*const source = async.eachSeries(
-    // Pass items to iterate over
-    ['campaign.sol', 'proxy.sol'],
-    // Pass iterator function that is called for each item
-    function(filename, cb) {
-        fs.readFile(filename, function(err, content) {
-            if (!err) {
-                response.write(content);
-            }
+//console.log("source Camp = ", source);
+//console.log("source token = ", sourceToken);
 
-            // Calling cb makes it go to the next item.
-            cb(err);
-        });
-    }
-);*/
-
-module.exports = solc.compile(source, 1).contracts[":Campaign"];
+//module.exports = solc.compile(source, 1).contracts; //esto funciona bien con un fichero
+module.exports = {
+    campaign: solc.compile(source, 1).contracts[":Campaign"],
+    proxy: solc.compile(sourceProxy, 1).contracts[":Proxy"],
+    token: solc.compile(sourceToken, 1).contracts[":FirstToken"]
+};
--- deploy.js
+++ deploy.js
@@ -1,9 +1,10 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');//se encarga de la conexion con la BC
 const Web3 = require('web3');//para trabajar con SC
-//const {interface, bytecode } = require('./compile');
-
 const contracts = require('./compile');
-console.log("contracts ", contracts);
+
+const {interface, bytecode } = contracts.campaign;
+
+//console.log("contracts ", contracts);
 
 //mi cuenta
 const provider = new HDWalletProvider(
@@ -17,10 +18,17 @@ const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account ', accounts[0]);
+    //console.log('Attempting to deploy from account ', accounts[0]);
+    //console.log("INTERFACE ", interface);
+    //console.log("BYTECODE ", bytecode);
 
-    const result = await new web3.eth.Contract(JSON.parse(contracts.campaign.interface))
-        .deploy({ data: contract.campaign.bytecode, arguments: ['We are starting our Campaing!!!!']})
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy(
+            {
+                data: bytecode
+                //arguments: ['We are starting our Campaing!!!!']
+            }
+        )
         .send({ gas: 5000000, from: accounts[0]});
 
     console.log('Contract deploy to ', result.options.address);
